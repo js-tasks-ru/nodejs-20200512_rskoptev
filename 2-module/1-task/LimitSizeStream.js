a1f5646109b3de6229a597cee0166662d601df74
MaMMoth Jr <rskoptev@gmail.com>
2020-05-24 15:35:07 +0300

    add solution for 2-module 1-task and 2-task

--- 2-module/1-task/LimitSizeStream.js
+++ 2-module/1-task/LimitSizeStream.js
@@ -1,30 +1,22 @@
-//sources
 const stream = require('stream');
 const fs = require('fs');
-const { Readable, Writable, Transform } = require('stream');
-//modules
 const LimitExceededError = require('./LimitExceededError');
 
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.limit = options.limit;
+    this.counterValue = 0;
   }
-
   _transform(chunk, encoding, callback) {
-
+    this.counterValue += chunk.length;
+    if (this.counterValue <= this.limit) {
+      callback(null, chunk);
+    } else {
+      callback(new LimitExceededError());
+    }
   }
 }
-
-const limitedStream = new LimitSizeStream({limit: 8}); // 8 байт
-const outStream = fs.createWriteStream('out.txt');
-
-limitedStream.pipe(outStream);
-
-limitedStream.write('hello'); // 'hello' - это 5 байт, поэтому эта строчка целиком записана в файл
-
-setTimeout(() => {
-  limitedStream.write('world'); // ошибка LimitExceeded! в файле осталось только hello
-}, 10);
 
 module.exports = LimitSizeStream;
 
--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -4,12 +4,25 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+     this.lastLine = '';
   }
 
   _transform(chunk, encoding, callback) {
+    let data = chunk.toString();
+    if (this.lastLine) {
+      data = this.lastLine + data;
+    }
+    const parts = data.split(os.EOL);
+    this.lastLine = parts.pop();
+    parts.forEach(part => this.push(part));
+    callback();
   }
 
   _flush(callback) {
+    if (this.lastLine) {
+      this.push(this.lastLine);
+   }
+   callback();
   }
 }
 
