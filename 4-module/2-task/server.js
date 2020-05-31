5a559fbcda508f2d73bb07cd8590639bbfefee9d
MaMMothJr <56677996+MaMMothJr@users.noreply.github.com>
2020-05-31 23:50:39 +0300

    add solution for 4-module 1,3-task

--- 4-module/2-task/server.js
+++ 4-module/2-task/server.js
@@ -10,72 +10,51 @@ server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
   const filepath = path.join(__dirname, 'files', pathname);
   const limitStream = new LimitSizeStream ({limit: 1048576});
-  const streamFile = fs.createWriteStream(filepath);
+  const writeFile = fs.createWriteStream(filepath,  {flags: 'wx'});
 
 
   switch (req.method) {
     case 'POST':
-      fs.createReadStream(filepath)
+      if (pathname.includes('/')) {
+        res.statusCode = 400;
+        res.end('Is a directory or wrong name');
+        return;
+      }
+      req
+        .pipe(limitStream)
+        .on('error', err => {
+          if (err.code === 'LIMIT_EXCEEDED') {
+            res.statusCode = 413;
+            fs.unlink(filepath, () => {});
+            res.end('Limit has been exceeded.');
+          }
+        })
+        .pipe(writeFile)
+        .on('close', () => {
+          if (req.aborted) {
+            writeFile.end();
+            fs.unlink(filepath);
+          } else {
+            res.statusCode = 201;
+            res.end('File created');
+          }})
         .on('error', error => {
           if (error.code === 'ENOENT') {
             res.statusCode = 404;
             res.end('No such file or directory');
-          } else if (error.code === 'EISDIR') {
-            res.statusCode = 400;
-            res.end('Don`t use /');
-          } else {
+          } else if (error.code === 'EEXIST') {
+            res.statusCode = 409;
+            res.end('File exists');
+          }  else {
             res.statusCode = 500;
-            res.end('internal server error');
-          }
-        })
-        .pipe(limitStream)
-        .pipe(streamFile);
+            res.end('Server error');
+          }});
+      break;
 
-  }});
-
-//       streamFile
-//         .on('close', () => {
-//         res.statusCode = 201;
-//         res.end('File created');
-//         })
-//         .on('error', err =>  {
-//         if (err.code === 'EEXIST') {
-//           res.statusCode = 409;
-//           res.end('File exists');
-//           return;
-//         } res.statusCode = 500;
-//         res.setHeader('Connection', 'close');
-//         res.end('Internal server error');
-//
-//         fs.unlink(filepath, (err) => {});
-//       });
-//
-//         limitStream.on('error', (err) => {
-//           if (err.code === 'LIMIT_EXCEEDED') {
-//             res.statusCode = 413;
-//             res.setHeader('Connection', 'close');
-//             res.end('File is too big');
-//
-//             fs.unlink(filepath, (err) => {});
-//             return;
-//           }
-//
-//           console.error(err);
-//
-//           res.statusCode = 500;
-//           res.setHeader('Connection', 'close');
-//           res.end('Internal server error');
-//
-//           fs.unlink(filepath, (err) => {});
-//         });
-//
-//
-//       break;
-//
-//     default:
-//       res.statusCode = 500;
-//       res.end('Not implemented');
-//   }
-// });
+    default:
+      res.statusCode = 501;
+      res.end('Not implemented');
+  }
+});
 
 module.exports = server;
