0dd886fc485781c9ea819423d5d5b92eebe878b2
MaMMothJr <rskoptev@gmail.com>
2020-06-02 13:30:23 +0300

    add solution for 4-module 1,2,3-task v4

--- 4-module/2-task/server.js
+++ 4-module/2-task/server.js
@@ -2,15 +2,14 @@ const url = require('url');
 const http = require('http');
 const path = require('path');
 const fs = require('fs');
-const LimitSizeStream = require('./limitSizeStream');
+const LimitSizeStream = require('./LimitSizeStream');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
+
   const pathname = url.parse(req.url).pathname.slice(1);
   const filepath = path.join(__dirname, 'files', pathname);
-  const limitStream = new LimitSizeStream ({limit: 1048576});
-  const writeFile = fs.createWriteStream(filepath,  {flags: 'wx'});
 
 
   switch (req.method) {
@@ -19,38 +18,36 @@ server.on('request', (req, res) => {
         res.statusCode = 400;
         res.end('Is a directory or wrong name');
         return;
-      }
+      } else {
+
+      const limitStream = new LimitSizeStream ({limit: 1048576});
+      const writeFile = fs.createWriteStream(filepath,{flags: 'wx'});
+
       req
         .pipe(limitStream)
         .on('error', err => {
           if (err.code === 'LIMIT_EXCEEDED') {
             res.statusCode = 413;
-            fs.unlinkSync(filepath);
+            fs.unlink(filepath, ()=>{});
             res.end('Limit has been exceeded.');
-          }
-        })
-        .pipe(writeFile)
-        .on('close', () => { //i tried using event:'aborted'
-          if (req.aborted) {
-            writeFile.end();
-            fs.unlinkSync(filepath);
-          } else {
-            res.statusCode = 201;
-            res.end('File created');
-            // console.log(req);
           }})
-        .on('error', error => {
-          if (error.code === 'ENOENT') {
-            res.statusCode = 404;
-            res.end('No such file or directory');
-          } else if (error.code === 'EEXIST') {
-            res.statusCode = 409;
-            res.end('File exists');
-          }  else {
-            res.statusCode = 500;
-            res.end('Server error');
-          }});
+        .pipe(writeFile)
+        .on('finish',() =>{
+          res.statusCode = 201;
+          res.end('File created');
+        })
+         .on('error', err => {
+           if (err.code === 'EEXIST') {
+             res.statusCode = 409;
+             res.end('File exists');
+           }
+         })
 
+      req
+        .on('aborted', () => {
+          fs.unlink(filepath,()=>{});
+          res.end('Connection error');
+       })}
       break;
 
     default:
